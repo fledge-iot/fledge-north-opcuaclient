fc761f1ea071bf2df5b9d885c4b528c95fdd4f1d
Mohit Tomar <m.singh19994@gmail.com>
2022-12-01 16:24:45 +0530

    Added requirements.sh file
    Signed-off-by: Mohit Tomar <m.singh19994@gmail.com>

---
+++ requirements.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+##---------------------------------------------------------------------------
+## Copyright (c) 2022 Dianomic Systems Inc.
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##---------------------------------------------------------------------------
+
+##
+## Author: Mohi SIngh Tomar
+##
+
+os_name=$(grep -o '^NAME=.*' /etc/os-release | cut -f2 -d\" | sed 's/"//g')
+os_version=$(grep -o '^VERSION_ID=.*' /etc/os-release | cut -f2 -d\" | sed 's/"//g')
+echo "Platform is ${os_name}, Version: ${os_version}"
+
+pygte37=$(python3 -c 'import sys; print("Y") if sys.version_info.major >= 3 and sys.version_info.minor >= 7 else print("N")')
+
+if [ pygte37 == "Y" ]
+then
+    python3 -m pip install asyncua
+else
+    echo "Requires platform with Python > = 3.7"
+    exit 1
+fi
