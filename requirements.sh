d5fe4504b758c0039a49a225e65a84ae90796309
Mohit Tomar <m.singh19994@gmail.com>
2022-12-01 19:40:17 +0530

    Refactored Code
    Signed-off-by: Mohit Tomar <m.singh19994@gmail.com>

--- requirements.sh
+++ requirements.sh
@@ -20,16 +20,14 @@
 ## Author: Mohi SIngh Tomar
 ##
 
-os_name=$(grep -o '^NAME=.*' /etc/os-release | cut -f2 -d\" | sed 's/"//g')
-os_version=$(grep -o '^VERSION_ID=.*' /etc/os-release | cut -f2 -d\" | sed 's/"//g')
-echo "Platform is ${os_name}, Version: ${os_version}"
 
 pygte37=$(python3 -c 'import sys; print("Y") if sys.version_info.major >= 3 and sys.version_info.minor >= 7 else print("N")')
 
-if [ ${pygte37} == "Y" ]
+if [ ${pygte37} == "N" ]
 then
-    python3 -m pip install asyncua
-else
-    echo "Requires platform with Python > = 3.7"
+    echo "Requires platform with Python >= 3.7"
     exit 1
+  
 fi
+
+python3 -m pip install -Ir python/requirements-opcuaclient.txt
