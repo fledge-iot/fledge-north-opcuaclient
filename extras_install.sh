ea65d7259ec8708973070a705d328cfb470f3267
ashish-jabble <ashish@dianomic.com>
2022-12-09 15:00:30 +0530

    DEBUG added
    Signed-off-by: ashish-jabble <ashish@dianomic.com>

--- extras_install.sh
+++ extras_install.sh
@@ -3,6 +3,10 @@
 ARCHITECTURE=$(arch)
 
 if [ ${ARCHITECTURE} = "armv7l" ]; then
+    sudo apt install -y libssl-dev
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh /dev/stdin -y
+    echo "BEFORE: PATH: "$PATH
     . "$HOME/.cargo/env"
+    echo "AFTER: PATH: "$PATH
+    rustc --version
 fi
