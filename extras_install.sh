3c5ec5703c91124867e95b49e9f54b35fc5a2fe9
ashish-jabble <ashish@dianomic.com>
2022-12-09 16:03:11 +0530

    removed sudo
    Signed-off-by: ashish-jabble <ashish@dianomic.com>

--- extras_install.sh
+++ extras_install.sh
@@ -3,7 +3,7 @@
 ARCHITECTURE=$(arch)
 
 if [ ${ARCHITECTURE} = "armv7l" ]; then
-    sudo apt install -y libssl-dev
+    apt install -y libssl-dev
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh /dev/stdin -y
     echo "BEFORE: PATH: "$PATH
     . "$HOME/.cargo/env"
